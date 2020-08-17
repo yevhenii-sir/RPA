4371c535c6b1f88bd5733da7083fc8285b1fa96a
Yevhenii Sirenko <ew_sirenko@ukr.net>
2020-08-17 21:17:10 +0300

    Version 0.6 Made a colorful menu

--- RPA_G/RPA_GAME.pas
+++ RPA_G/RPA_GAME.pas
@@ -9,22 +9,56 @@ BotleHeal: array[1..3] of integer;
 // процедуры
 // процедура для вызова надписей команд 
 // обучения 
+
+// процедура смены цвета консоли 
+Procedure chooseColor();
+begin
+//доработать функцию смены цввета
+ Writeln('Смена цвета'); 
+end;
+//процедура старту игры
+procedure starting();
+begin
+  //Главнвя переменая для урона теперь WeaponDamage
+  TextColor(LightBlue);
+  PlayerDamage:= 5;
+  Writeln('------------------');
+  Writeln('НАЧАЛО');
+  Writeln('------------------');
+  Writeln('Вибирете уровень сложности');
+  Writeln('1-легко');
+  Writeln('2-нормальна');
+  Writeln('3-тяжолоя');
+end;
+
 procedure ConsoleHelp();
 begin
+ TextColor(red);
  Writeln('Вся игра состоит из команд до главнои консоли');
+ TextColor(LightBlue);
  Writeln('Характеристики_игрока - показывает характеристику игрока ');
+ TextColor(red);
  Writeln('Инвентарь - показывает оружие и батончики которые есть в наличии');
+ TextColor(LightBlue);
  Writeln('Оружие - выбор оружия');
+ TextColor(red);
  Writeln('Качаться - увелечения урона для любого з оружий, но если есть баночек силы ');
+ TextColor(LightBlue);
  Writeln('Лечиться - Востановления здоровья игрока, в зависимости от баночки');
+ TextColor(red);
  Writeln('Бой - начатия боя з мобами ');
+ TextColor(LightBlue);
  Writeln('Не стоить запоминать все команды, если что-то забудешь напиши help в консоль');
+ TextColor(red);
  Writeln('Очистить - очищает екран, от всех событий(текста)');
+ Writeln('Сменить_цвет - позволяет выбрать цвет текста');
+ TextColor(LightBlue);
 end;
 
 //пороцедура для вывода содержимого инвентаря
 procedure Invent();
 begin
+  TextColor(Yellow);
   Writeln('Оружие: ');
   Writeln('    Меч');
   Writeln('    Кинжал');
@@ -49,6 +83,7 @@ end;
 //процедура для увеличения урона в оружия
 procedure UpDamageWeap();
   begin
+    TextColor(Magenta);
     Writeln('Выбирите оружие которое вам нужно улутшить: ');
     Writeln('   Меч - ', Weapons[1], 'урона       +', WeaponUp);
     Writeln('   Кинжал - ', Weapons[2], ' урона   +', WeaponUp);
@@ -104,6 +139,7 @@ procedure UpDamageWeap();
 //процедура для выбора оружия
 procedure weapon();
 begin
+  TextColor(Magenta);
   Writeln('Выбирите оружие: ');
   Writeln('   Меч - ', Weapons[1], 'урона');
   Writeln('   Кинжал - ', Weapons[2], ' урона');
@@ -228,7 +264,6 @@ procedure Comand(ComandAnswer:string);
 begin
   Case ComandAnswer of 
     'help': ConsoleHelp();
- 
     'Характеристики_игрока':
     begin
       Writeln('Здоровье игрока - ', PlayerHeal);
@@ -244,23 +279,16 @@ begin
      'Лечиться': UpHeal();
      'Конец':conez();
      'Очистить':cleanerText();
+     'Сменить_цвет':chooseColor();
     else Writeln('Ошибка');
   end;
 end;
 
 // Основная программа
 begin
-  //Главнвя переменая для урона теперь WeaponDamage
-  PlayerDamage:= 5;
-  Writeln('------------------');
-  Writeln('НАЧАЛО');
-  Writeln('------------------');
-  Writeln('Вибирете уровень сложности');
-  Writeln('1-легко');
-  Writeln('2-нормальна');
-  Writeln('3-тяжолоя');
-  Readln(diferent);
-  differ(diferent);
+ starting();
+ Readln(diferent);
+ differ(diferent);
  Writeln('Начнём наше путишествия');
  Writeln('обучения');
  ConsoleHelp();
